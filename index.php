fd09188b8e7398bcb3f842078577fdf0b87cc444
a-amirfarzam <a.amirfarzam@gmail.com>
2022-04-15 20:35:54 +0430

    Start Design

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Dynamic Layout Design</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
